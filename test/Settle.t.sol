77df3d4039bbbcd4434b814e9a4ee08e4599d9dc
eacent <eacentcheung@gmail.com>
2022-03-15 19:31:23 +0800

    add settle test

---
+++ test/Settle.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import "../contracts/interfaces/IERC20.sol"; 
+import "../contracts/Settle.sol";
+
+contract SettleTest is DSTest {
+    Settle settle;
+    address public chip = 0xfd29982Fa0b7d3eDcaE9B0c49D350C07c7cEC5c1;
+    address public currency = 0xe19B95fB3bDE006E436c7C83DCb8018D55671490;
+    address public sam = 0x16EAF3c5201F57A9cc0B35688269c082e215627c;
+    
+    function setUp() public {
+        settle = new Settle(currency);
+    }
+
+    function testExample() public {
+        assertTrue(true);
+    }
+
+    function testSettleOwner() public {
+        assertEq(settle.owner(), address(this));
+    }
+
+    function testCurrency() public {
+        assertEq(settle.getCurrency(), currency);
+    }
+    
+    function testAddWalletToken() public {
+        assertTrue(settle.addWalletToken(chip));
+    }
+
+    function testGetWalletSettle() public {
+        settle.addWalletToken(chip);
+        uint chipBalance = IERC20(ship).balanceOf(sam);
+        assertEq(settle.getWalletSettle(sam), chipBalance);
+    }
+}
